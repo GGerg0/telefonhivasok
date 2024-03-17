8c7aca919960d8508c60f5a38e21d6ed91380d0c
Gecse Gergő <gecsegergo006@gmail.com>
2024-03-17 13:36:42 +0100

    4. feladat

--- telefonosprojekt.py
+++ telefonosprojekt.py
@@ -1,5 +1,5 @@
 def mpbe(o,p,mp):
-    return o*3600+p*60+mp
+    return int(o*3600+p*60+mp)
 
 '''o = int(input())
 p = int(input())
@@ -10,9 +10,11 @@ print(mpbe(o,p,mp))'''
 f= open("hivas.txt", "rt")
 
 orak = {}
+adatok = []
 
 for sor in f:
     sor = sor.strip().split(" ")
+    adatok.append(sor)
     if sor[0] in orak:
         orak[sor[0]] += 1
     else:
@@ -22,3 +24,11 @@ for sor in f:
 print("3. Feladat")
 for k,v in orak.items():
     print(f"{k} ora {v} hivas")
+
+leghosszabb = [0,0]
+for hivasok in range (len(adatok)):
+    masodperc = mpbe(int(adatok[hivasok][3]),int(adatok[hivasok][4]),int(adatok[hivasok][5])) - mpbe(int(adatok[hivasok][0]),int(adatok[hivasok][1]),int(adatok[hivasok][2]))
+    if leghosszabb[1] < masodperc:
+        leghosszabb = hivasok+1,masodperc
+
+print(f"A leghosszabb ideig vonalban levo hivo {leghosszabb[0]}. sorban szerepel,\n a hivas hossza: {leghosszabb[1]} masodperc")
