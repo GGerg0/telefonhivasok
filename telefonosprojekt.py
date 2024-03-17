58864cdcb79c3bcff6cfad1cdb2fe7bdebc84a8c
Gecse Gergő <gecsegergo006@gmail.com>
2024-03-17 12:58:15 +0100

    3. feladat

--- telefonosprojekt.py
+++ telefonosprojekt.py
@@ -1,12 +1,24 @@
 def mpbe(o,p,mp):
     return o*3600+p*60+mp
 
-o = int(input())
+'''o = int(input())
 p = int(input())
 mp = int(input())
 
-print(mpbe(o,p,mp))
+print(mpbe(o,p,mp))'''
 
 f= open("hivas.txt", "rt")
-f.readline()
 
+orak = {}
+
+for sor in f:
+    sor = sor.strip().split(" ")
+    if sor[0] in orak:
+        orak[sor[0]] += 1
+    else:
+        orak[sor[0]] = 1
+
+
+print("3. Feladat")
+for k,v in orak.items():
+    print(f"{k} ora {v} hivas")
